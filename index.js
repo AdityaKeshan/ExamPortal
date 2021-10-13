8cceaf64ec06a61f4211374805ff428de2089c78
Sapphire2664 <aditya.keshan2019@vitstudent.ac.in>
2021-10-14 04:03:20 +0530

    Added Passport module and OAuth2 with guidelines

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 let express = require("express");
 let bodyParser = require("body-parser");
+let passport=require('passport')
+let GoogleStrategy= require('passport-google-oauth').OAuth2Strategy
+passport.use(new GoogleStrategy())
 const path = require("path");
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "ejs");
+app.use(passport.initialize())
+app.use(passport.session())
 
 //set up front-end file
 app.use(express.static(path.resolve(__dirname, "./frontend/build")));
@@ -22,3 +27,5 @@ app.get("*", (req, res) => {
 app.get("/", function (req, res) {
   res.send("Okay!");
 });
+// TODO: We can basically start with requiring passport and passport-google-auth
+// Specify the route and store the user info but before we needs to test it with a frontend 
