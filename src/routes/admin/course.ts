5f2c45aa13abf33ef8bec237fda5beaab9cd8a56
Sapphire2664 <aditya.keshan2019@vitstudent.ac.in>
2021-11-10 01:31:30 +0530

    Added storage structure

--- src/routes/admin/course.ts
+++ src/routes/admin/course.ts
@@ -1,35 +1,42 @@
-import express from "express" 
+import express from "express";
 const router = express.Router();
-import {admin,database} from "../../config/firebase-config"
+import { app, admin, database } from "../../config/firebase-config";
 // const { app, admin, database } = require(path.resolve(
 //   "../ExamPortal/src/config/firebase-config"
 // ));
-import {set,ref} from "firebase/database"
-import { Request, Response, NextFunction } from 'express';
+import { set, ref } from "firebase/database";
+import { Request, Response, NextFunction } from "express";
+import { DecodedIdToken } from "firebase-admin/lib/auth/token-verifier";
 
-const verify = function (req:Request, res:Response, next:NextFunction):void {
+const verify = function (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): void {
   const { tokenId } = req.body;
   admin
     .auth()
     .verifyIdToken(tokenId)
-    .then(() => {
+    .then((decodedToken: DecodedIdToken) => {
+      req.body["uid"] = decodedToken.uid;
       return next();
     })
-    .catch((error:Error) => {
+    .catch((error: Error) => {
       res.status(400);
       res.json({ message: "verification failed!", err: error });
     });
 };
 
-router.get("/", (req:Request, res:Response) => {
+router.get("/", (req: Request, res: Response) => {
   res.send("Hello");
 });
-router.post("/", verify, (req:Request, res:Response) => {
+// TODO:  Courses need to have more attributes
+router.post("/", verify, (req: Request, res: Response) => {
   console.log("here");
-  let courseId:string = Math.random().toString(); //TODO : - use uuid
+  let uid = req.body["uid"];
+  let courseId: string = ((Math.random() * 10000 + 1) | 0).toString(); //TODO : - use uuid
   const { courseName, admin } = req.body;
-  set(ref(database, "admin/" + "1"), {
-    courseId: courseId,
+  set(ref(database, "admin/" + uid + "/courses/" + courseId), {
     courseName: courseName,
     admin: admin,
   });
